9ae136bc3b2925e95ff9eb5ac4fb6f4d2c8da9ac
RayGoncalves21 <raygoncalves21@gmail.com>
2022-10-22 23:15:39 -0300

    renomeando para project

--- recipes/models.py
+++ recipes/models.py
@@ -46,10 +46,10 @@ class Recipe(models.Model):
     cover = models.ImageField(
         upload_to='recipes/covers/%Y/%m/%d/', blank=True, default='')
     category = models.ForeignKey(
-        Category, on_delete=models.SET_NULL, null=True
+        Category, on_delete=models.SET_NULL, null=True, blank=True, default=None,
     )
     author = models.ForeignKey(
-        User, on_delete=models.SET_NULL, null=True
+        User, on_delete=models.SET_NULL, null=True, blank=True, default=None,
     )
 
     def __str__(self):
